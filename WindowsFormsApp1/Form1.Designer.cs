a63b6a21244754e00046d2bbb1a1a7bf1d49b4f4
MoonRock28 <ste11073@byui.edu>
2018-03-12 16:57:48 -0600

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sideA = new System.Windows.Forms.NumericUpDown();
+            this.sideB = new System.Windows.Forms.NumericUpDown();
+            this.sideC = new System.Windows.Forms.NumericUpDown();
+            this.results = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sideA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sideB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sideC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.875F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.125F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.sideA, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.sideB, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.sideC, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.results, 2, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Side c length: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Side b length: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 100);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Side a length: ";
+            // 
+            // sideA
+            // 
+            this.sideA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sideA.Location = new System.Drawing.Point(122, 3);
+            this.sideA.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.sideA.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.sideA.Name = "sideA";
+            this.sideA.Size = new System.Drawing.Size(195, 26);
+            this.sideA.TabIndex = 4;
+            this.sideA.KeyUp += new System.Windows.Forms.KeyEventHandler(this.sideA_ValueChanged);
+            // 
+            // sideB
+            // 
+            this.sideB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sideB.Location = new System.Drawing.Point(122, 103);
+            this.sideB.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.sideB.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.sideB.Name = "sideB";
+            this.sideB.Size = new System.Drawing.Size(195, 26);
+            this.sideB.TabIndex = 5;
+            this.sideB.KeyUp += new System.Windows.Forms.KeyEventHandler(this.sideB_ValueChanged);
+            // 
+            // sideC
+            // 
+            this.sideC.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sideC.Location = new System.Drawing.Point(122, 203);
+            this.sideC.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.sideC.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.sideC.Name = "sideC";
+            this.sideC.Size = new System.Drawing.Size(195, 26);
+            this.sideC.TabIndex = 6;
+            this.sideC.KeyUp += new System.Windows.Forms.KeyEventHandler(this.sideC_ValueChanged);
+            // 
+            // results
+            // 
+            this.results.AutoSize = true;
+            this.results.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.results.Location = new System.Drawing.Point(323, 300);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(474, 150);
+            this.results.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "TriangleClassifier";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sideA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sideB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sideC)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown sideA;
+        private System.Windows.Forms.NumericUpDown sideB;
+        private System.Windows.Forms.NumericUpDown sideC;
+        private System.Windows.Forms.Label results;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int[] minToMax;
+        private int[] values = { 0, 0, 0};
+
+        private bool isTriangle = false;
+        private bool isIsosceles = false;
+        private bool isEquilateral = false;
+        private bool isRight = false;
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void sideA_ValueChanged(object sender, EventArgs e)
+        {
+            if (sideA.Value < 0)
+            {
+                sideA.Value = sideA.Value * -1;
+            }
+
+            hasValues();
+        }
+
+        private void sideB_ValueChanged(object sender, EventArgs e)
+        {
+            if (sideB.Value < 0)
+            {
+                sideB.Value = sideB.Value * -1;
+            }
+
+            hasValues();
+        }
+
+        private void sideC_ValueChanged(object sender, EventArgs e)
+        {
+            if (sideC.Value < 0)
+            {
+                sideC.Value = sideC.Value * -1;
+            }
+
+            hasValues();
+        }
+
+        private void hasValues()
+        {
+            isTriangle = false;
+            isIsosceles = false;
+            isEquilateral = false;
+            isRight = false;
+
+            runChecks();
+        }
+
+        private void runChecks()
+        {
+            values[0] = (int)sideA.Value;
+            values[1] = (int)sideB.Value;
+            values[2] = (int)sideC.Value;
+
+            minToMax = values.OrderBy(i => i).ToArray();
+
+            checkTriangle();
+
+            if (isTriangle)
+            {
+                checkEquilateral();
+                checkIsosceles();
+                checkRight();
+            }
+
+            //display the triangle results.
+            displayResults();
+        }
+
+        private void displayResults()
+        {
+            String result;
+            if (isTriangle)
+            {
+                if (isEquilateral)
+                {
+                    result = "These side lengths produce a valid equilateral triangle.";
+                }
+                else if (isIsosceles)
+                {
+                    result = "These side lengths produce a valid Isosceles triangle.";
+                }
+                else if (isRight)
+                {
+                    result = "These side lenghts produce a valid right triangle.";
+                }
+                else
+                {
+                    result = "These side lenghts produce a triangle.";
+                }
+            }
+            else
+            {
+                result = "These side lenghts do not produce a triangle.";
+            }
+
+            results.Text = result;
+        }
+
+        private void checkTriangle()
+        {
+            if (minToMax[0] + minToMax[1] > minToMax[2])
+            {
+                isTriangle = true;
+            }
+        }
+
+        private void checkEquilateral()
+        {
+            if (minToMax[0] == minToMax[1] &&
+                minToMax[1] == minToMax[2])
+            {
+                isEquilateral = true;
+            }
+        }
+
+        private void checkIsosceles()
+        {
+            if (minToMax[0] == minToMax[1] ||
+                minToMax[1] == minToMax[2])
+            {
+                isIsosceles = true;
+            }
+        }
+
+        private void checkRight()
+        {
+            if ((minToMax[0] * minToMax[0]) + (minToMax[1] * minToMax[1]) ==
+                (minToMax[2] * minToMax[2]))
+            {
+                isRight = true;
+            }
+        }
+    }
+}
